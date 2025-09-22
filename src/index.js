18dbff1f8d07de663811039b089030869b59681f
Tatiana C. Molina Higgins <73068033+space-schnoodle@users.noreply.github.com>
2025-09-22 11:32:55 -0500

    Update
    Update and delete some files.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+// 1) Import the React and ReactDOM libraries
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// 2) Get a reference to the div with ID root
+const el = document.getElementById('root');
+
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a component
+function App() {
+  // const date = new Date();
+  // const time = date.toLocaleTimeString();
+
+  return <h1>{new Date().toLocaleTimeString()}</h1>;
+}
+
+// 5) Show the component on the screen
+root.render(<App />);
